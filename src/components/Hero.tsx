d2b711605a5df159c5cc09361bfc35372d0dfd73
Milan721166 <milansahoo7211662005@gmail.com>
2025-12-23 14:58:25 +0530

    fixedissue of module

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,23 +14,28 @@ export function Hero() {
       {/* Hero section */}
       <section className="w-full min-h-[80vh] bg-[#0055ff] py-28 relative z-0">
         <div className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row items-center pl-4 pr-0 gap-10">
+          
           {/* Left Side with GRID Background */}
           <div className="w-full md:w-3/4 flex justify-center items-center">
             <div
               className="w-full max-w-4xl p-4 bg-[linear-gradient(rgba(255,255,255,0.1)_1px,_transparent_3px),linear-gradient(90deg,rgba(255,255,255,0.1)_1px,_transparent_3px)] bg-[size:50px_50px] bg-[position:25px_25px]"
             >
               <div className="text-base text-white font-semibold uppercase py-6">
-                India’s Front-running Startup
+                India&apos;s Front-running Startup
               </div>
+
               <h1 className="text-3xl md:text-4xl font-bold text-white leading-tight pb-8 pt-1">
                 Revolutionizing Exam Preparation with Tech Innovation
               </h1>
+
               <p className="text-2xl text-white pb-4">
                 Your Journey, Our Spark of Innovation
               </p>
+
               <p className="text-white text-base">
-                AI personalized guidance and support to help you spark on exam preparation with world's top educational opportunities.
+                AI personalized guidance and support to help you spark on exam preparation with world&apos;s top educational opportunities.
               </p>
+
               <div className="py-4">
                 <Link href="/#about">
                   <button className="mt-4 px-6 py-2 bg-indigo-700 text-white rounded-lg hover:bg-indigo-800 duration-500 transition-colors">
