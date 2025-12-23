3898fd5138c169b5881c444c423178ae9e51dee5
Milan721166 <milansahoo7211662005@gmail.com>
2025-12-23 17:38:13 +0530

    fix: production build errors (typescript + eslint)

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -4,18 +4,32 @@ import * as React from "react";
 
 export function About() {
   return (
-    <div id="about" className="px-6 pb-12 pt-32 bg-white">
-      <div className="text-center mb-8">
-        <div className="inline-block bg-blue-200 text-blue-700 font-bold px-6 py-2 rounded-full text-lg shadow">
-          About Us
+    <>
+      <div className="px-6 pb-12 pt-32 bg-white">
+        <div className="text-center mb-8">
+          <div className="inline-block bg-blue-200 text-blue-700 font-bold px-6 py-2 rounded-full text-lg shadow">
+            About Us
+          </div>
         </div>
-      </div>
 
-      <p className="max-w-6xl mx-auto text-center text-gray-700 text-base sm:text-lg leading-relaxed">
-        At SynergiaPrep Pvt Ltd, we empower students with the tools, guidance, and global opportunities they need to succeed — whether it’s preparing for competitive exams or securing admission to top international universities.
-        Our platform brings together the best of both worlds: expert-led online exam preparation for entrance and government exams like JEE, NEET, WBJEE, and more, along with personalized study abroad consultancy that opens doors to world-class institutions.
-        With a team of experienced mentors, dynamic learning resources, and transparent guidance at every step, SynergiaPrep is your one-stop solution for academic success both at home and abroad.
-      </p>
+        <p className="max-w-6xl mx-auto text-center text-gray-700 text-base sm:text-lg leading-relaxed">
+          At SynergiaPrep Pvt Ltd, we empower students with the tools, guidance,
+          and global opportunities they need to succeed — whether it&apos;s
+          preparing for competitive exams or securing admission to top
+          international universities.
+          <br />
+          <br />
+          Our platform brings together the best of both worlds: expert-led online
+          exam preparation for entrance and government exams like JEE, NEET,
+          WBJEE, and more, along with personalized study abroad consultancy that
+          opens doors to world-class institutions.
+          <br />
+          <br />
+          With a team of experienced mentors, dynamic learning resources, and
+          transparent guidance at every step, SynergiaPrep is your one-stop
+          solution for academic success both at home and abroad.
+        </p>
+      </div>
 
       <div className="px-6 py-8 flex flex-wrap justify-center gap-12 bg-white text-center">
         <div>
@@ -35,7 +49,7 @@ export function About() {
           <p className="text-gray-700">Global Universities Partnered</p>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
